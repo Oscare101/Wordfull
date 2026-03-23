40f5e8b791db19d25489b1979de3ef2f981c56c2
Oscare101 <kirillfedortsev2000@gmail.com>
2026-03-23 15:47:57 +0200

    word pack name fix

--- src/screens/SettingsScreen.tsx
+++ src/screens/SettingsScreen.tsx
@@ -1,22 +1,23 @@
-import { Button, FlatList, StyleSheet, Text, View } from 'react-native';
-import React, { useCallback, useEffect, useState } from 'react';
+import { FlatList, StyleSheet, View } from 'react-native';
+import React, { useCallback } from 'react';
 import { RootStackParamList } from '../navigation/types';
 import { StackScreenProps } from '@react-navigation/stack';
 import SimpleHeader from '../components/global/SimpleHeader';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
-import { Language } from '../constants/interfaces/interface';
+import { WordPack } from '../constants/interfaces/interface';
 import text from '../constants/languages/text';
 import { useSettings } from '../context/SettingsContext';
 import colors from '../constants/themes/colors';
 import { IconName } from '../constants/interfaces/iconInterface';
 import SettingsButtonItem from '../components/settings/SettingsButtonItem';
-import { getSystemWordPackById } from '../constants/wordPacks/wordPack';
+import { useSelectedWordPack } from '../constants/wordPacks/useSelectedWordPack';
 
 type Props = StackScreenProps<RootStackParamList, 'SettingsScreen'>;
 
 export default function SettingsScreen({ navigation }: Props) {
   const insets = useSafeAreaInsets();
   const { language, theme, selectedWordPackId } = useSettings();
+  const wordPack: WordPack = useSelectedWordPack();
 
   const buttons: { title: string; icon: IconName; onPress?: () => void }[] = [
     {
@@ -34,8 +35,7 @@ export default function SettingsScreen({ navigation }: Props) {
       }, [navigation]),
     },
     {
-      title:
-        getSystemWordPackById(selectedWordPackId)?.name || selectedWordPackId,
+      title: wordPack?.name || selectedWordPackId,
       icon: 'list',
     },
     {
